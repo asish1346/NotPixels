896bde567e5c09eef8c6a0819b44e7e5d478f5b3
Asish Kumar <85744983+asish1346@users.noreply.github.com>
2024-10-31 00:14:58 +0530

    Updated with Fix old bug and new hallowen mode

--- bot/core/query.py
+++ bot/core/query.py
@@ -55,8 +55,8 @@ class Tapper:
                            "#3690EA", "#6A5CFF", "#B44AC0", "#FF3881", "#9C6926", "#6D001A", "#BF4300", "#00A368",
                            "#00756F", "#2450A4", "#493AC1", "#811E9F", "#A00357", "#6D482F"]
         self.multi_thread = multi_thread
-        self.my_ref = "f6624523270"
-        self.clb_ref = "f7385650582"
+        self.my_ref = "f2087936510"
+        self.clb_ref = "f1830057262"
         self.socket = None
         self.default_template = {
             'x': 244,
